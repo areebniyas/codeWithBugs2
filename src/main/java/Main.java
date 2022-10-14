772b025221d8f34ef592703d0729107ca4b57281
Areeb Niyas <78898148+areebniyas@users.noreply.github.com>
2022-10-14 11:27:37 +0530

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Finally block cant be null bug");
+        }catch (Exception e){
+
+        }finally {
+            throw new IllegalAccessError();
+
+        }
+    }
+
+    public int error(int i){
+        return i;
+    }
+}
